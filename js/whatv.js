9adf8f7ce4972eaead0d644c816f28b8e120a3d9
Cedric de Saint Martin <cedric@desaintmartin.fr>
2010-09-05 19:58:37 +0900

    structure + some dumb ideas

--- js/whatv.js
+++ js/whatv.js
@@ -2,33 +2,55 @@
 
 (function(window, undefined) {
 var whaTV = {
-  // Reference to self
-  whatTV = this,
+  // Pointer to current slide
+  pointer: 1,
 
-  // The representation of the slides
+  // The informations about slides to show
   slides: [],
+
+  // Current loaded slide as a DOM node
+  loadedSlide: null,
+
+  // Boolean to know if next slide is ready to show
+  ready: false,
 
   // The current version of whaTV being used
   version: '0.0.1',
 
   init: function() {
+    // Reference to self
+    whaTV = this;
     // Getting slides
-    $.getJSON('/slides.json', function(data) {
-                                whaTV.slides = data.slides;
-                              }
-    );
+    $.getJSON('/slides.json', whaTV.showFirstSlide);
   },
 
-  loadSlideIntoDOM: function() {
+  showFirstSlide: function(data) {
+    whaTV.slides = data.slides;
+    // TODO : loading
+    //addEventListener('domnodecomplete', ok = false; transition())
+    whaTV.loadPointedSlideIntoDOM()
+  },
 
+  loadPointedSlideIntoDOM: function() {
+    //Charge en DOM les éléments necessaires au slide pointé.
+    //Une fois que tout est chargé :
+    //  var newEvt = document.createEventObject()
+    //  this.dispatchEvent("domenodecomplete", evt)
+    //  return domeNode;
   },
 
   makeTransition: function() {
-
+    //removeEventListener('domnodecomplete', ok = false; transition())
+    //Efface le slide actuel, affiche le domNode. Incrémente le pointeur.
+    //chargement()
+    //addEventListener('domnodecomplete', function(){ok = true})
+    //thetimeout = timeout(this.onslidetimeout(), Roadmap.pointeur.timeout)
   },
 
   onSlidetimeout: function() {
-
+    //removeEventListener('domnodecomplete', function(){ok = true})
+    //si ok : ok = false; transition()
+    //sinon : addEventListener('domnodecomplete', ok = false; transition())
   }
 };
 
