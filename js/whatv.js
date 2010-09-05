815bbe2715522da5413b40a0781a7412ba1e9c02
Cedric de Saint Martin <cedric@desaintmartin.fr>
2010-09-05 15:51:09 +0900

    Starting from scratch, trying to do some clean object before beginning logic

---
+++ js/whatv.js
@@ -0,0 +1,36 @@
+'use strict'
+
+(function( window, undefined ) {
+var whaTV = function() {
+            // The whaTV object is actually just the init constructor 'enhanced'
+            return new whaTV.fn();
+          },
+// Map over jQuery in case of overwrite
+_whatTV = window.whaTV,
+// Use the correct document accordingly with window argument (sandbox)
+document = window.document,
+
+whaTV.fn = whaTV.prototype = {
+  init: function() {
+    
+    return this;
+  },
+  // The current version of whaTV being used
+  whaTV: "0.0.1",
+
+  // Here come all the functions and vars needed in whaTV
+};
+
+// Give the init function the whaTV prototype for later instantiation
+whaTV.fn.init.prototype = whaTV.fn;
+
+whaTV.extend = whaTV.fn.extend = function() {
+
+};
+
+
+$.getJSON('/slides.json', function(data){
+                              
+});
+
+})(window);
